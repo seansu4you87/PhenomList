b2d5e99f98e37ab637e1763e24ee9b067030d916
Sean Yu <seansu4you87@gmail.com>
2011-12-23 01:53:57 -0800

    actually adding PLURL files

---
+++ PhenomList/Model/PLURL.h
@@ -0,0 +1,13 @@
+//
+//  PLURL.h
+//  PhenomList
+//
+//  Created by Sean Yu on 12/23/11.
+//  Copyright (c) 2011 Blackboard Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PLURL : NSObject
+
+@end
---
+++ PhenomList/Model/PLURL.m
@@ -0,0 +1,13 @@
+//
+//  PLURL.m
+//  PhenomList
+//
+//  Created by Sean Yu on 12/23/11.
+//  Copyright (c) 2011 Blackboard Mobile. All rights reserved.
+//
+
+#import "PLURL.h"
+
+@implementation PLURL
+
+@end
